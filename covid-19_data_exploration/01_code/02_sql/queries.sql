1461477d85e8181b425c81d649f8656402996ef4
benlyj <benlimyujin@gmail.com>
2021-10-10 22:02:53 +0800

    edited readme and dashboard

--- covid-19_data_exploration/01_code/02_sql/queries.sql
+++ covid-19_data_exploration/01_code/02_sql/queries.sql
@@ -1,3 +1,4 @@
+-- Create covid_deaths table
 CREATE TABLE covid_deaths(
 	iso_code VARCHAR(50),
 	continent VARCHAR(50),
@@ -27,6 +28,7 @@ CREATE TABLE covid_deaths(
 	weekly_hosp_admissions_per_million NUMERIC(20,3)	
 );
 
+-- Create covid_vaccinations table
 CREATE TABLE covid_vaccinations(
 	iso_code VARCHAR(50),
 	continent VARCHAR(50),
@@ -73,78 +75,185 @@ CREATE TABLE covid_vaccinations(
 	excess_mortality_cumulative_per_million NUMERIC(20,3)
 );
 
--- check imports
+-- Check imports
 SELECT * FROM covid_deaths;
 SELECT * FROM covid_vaccinations;
 
--- select data to use
-SELECT continent, location, date, total_cases, new_cases, total_deaths, population 
+-- Select data to use
+SELECT 
+	continent, 
+	location, 
+	date, 
+	total_cases, 
+	new_cases, 
+	total_deaths, 
+	population 
 FROM covid_deaths
 ORDER BY location, date;
 
--- compare total cases vs total deaths for SG (death rate)
--- shows likelihood of dying if you contract covid
-SELECT location, date, total_cases, total_deaths, ROUND(total_deaths/total_cases*100,3) AS death_rate
+-- See how the death rate in SG has been changing; compare total cases vs total deaths
+SELECT 
+	location, 
+	date, 
+	total_cases, 
+	total_deaths, 
+	ROUND(total_deaths/total_cases*100,3) AS death_rate
 FROM covid_deaths
 WHERE location ILIKE '%singapore%'
 ORDER BY location, date;
 
--- look at total cases vs population (infection rate) in SG
--- shows what percentage of the SG population has contracted covid
-SELECT location, date, total_cases, population, ROUND(total_cases/population*100,3) AS infection_rate
+-- Show SG's latest death rate; choosing MAX(total_cases) is equivalent to choosing the latest (viz 1)
+-- Shows likelihood of dying if you contract covid
+SELECT 
+	location, 
+	MAX(total_cases) AS total_cases, 
+	MAX(total_deaths) AS total_deaths, 
+	ROUND(MAX(total_deaths)/MAX(total_cases)*100,2) AS death_rate
+FROM covid_deaths
+GROUP BY location
+HAVING location ILIKE '%singapore%';
+
+-- Look at total cases vs population (infection rate) in SG
+-- Shows what percentage of the SG population has contracted covid
+SELECT 
+	location, 
+	date, 
+	total_cases, 
+	population, 
+	ROUND(total_cases/population*100,3) AS infection_rate
 FROM covid_deaths
 WHERE location ILIKE '%singapore%'
 ORDER BY location, date;
 
--- check countries with highest infection rates (total cases vs population)
--- filter out rows with null in the continent column because these rows will have the location column filled with the continent
+-- Check countries with highest infection rates; i.e. total cases vs population (viz 3)
+-- Filter out rows with null in the continent column because these rows will have the location column filled with the continent;
 -- i.e. select rows where the continent column is not null because this filters out the continent level aggregation
-SELECT location, MAX(total_cases) AS highest_infection_count, population, ROUND(MAX(total_cases)/population*100,3) AS max_infection_rate
+SELECT 
+	location, 
+	MAX(total_cases) AS max_infection_count, 
+	population, 
+	ROUND(MAX(total_cases)/population*100,3) AS max_infection_rate
 FROM covid_deaths
 WHERE continent IS NOT NULL
 GROUP BY location, population
 HAVING (MAX(total_cases), population, MAX(total_cases)/population) IS NOT NULL
 ORDER BY max_infection_rate DESC;
 
--- check countries with the highest death count
--- filter out rows with null in the continent column because these rows will have the location column filled with the continent
+-- Check country infection rates over time (viz 4)
+-- Fill nulls with 0 in results using CASE
+SELECT 
+	location,
+	date,
+	population,
+	total_cases,
+	ROUND(total_cases/population*100,2) AS infection_rate,
+	CASE
+		WHEN total_cases IS NULL THEN 0
+		WHEN total_cases/population*100 IS NULL THEN 0
+		ELSE ROUND(total_cases/population*100,2)
+	END AS infection_rate_no_nulls
+FROM covid_deaths
+WHERE continent IS NOT NULL
+ORDER BY location, date;
+	
+
+-- Check countries with the highest death count
+-- Filter out rows with null in the continent column because these rows will have the location column filled with the continent;
 -- i.e. select rows where the continent column is not null because this filters out the continent level aggregation
-SELECT location, MAX(total_deaths) AS total_deaths
+SELECT 
+	location, 
+	MAX(total_deaths) AS total_deaths
 FROM covid_deaths
 WHERE continent IS NOT NULL
 GROUP BY location
 HAVING MAX(total_deaths) IS NOT NULL
 ORDER BY MAX(total_deaths) DESC;
 
--- check the death count by continent
--- select rows where the continent column is null because these rows have the location column filled with the continent
-SELECT location, MAX(total_deaths) AS total_deaths
+-- Check the death count by region (viz 2)
+-- Select rows where the continent column is null because these rows have the location column filled with the continent
+SELECT 
+	location, 
+	MAX(total_deaths) AS total_deaths
 FROM covid_deaths
 WHERE continent IS NULL
 GROUP BY location
 HAVING MAX(total_deaths) IS NOT NULL
+AND location NOT IN ('European Union', 'International')
 ORDER BY MAX(total_deaths) DESC;
 
--- check global death rate
-SELECT location, MAX(total_cases) AS total_cases, MAX(total_deaths) AS total_deaths, ROUND(MAX(total_deaths)/MAX(total_cases)*100,2) AS death_rate
+-- Check global death rate
+SELECT 
+	location, 
+	MAX(total_cases) AS total_cases, 
+	MAX(total_deaths) AS total_deaths, 
+	ROUND(MAX(total_deaths)/MAX(total_cases)*100,2) AS death_rate
 FROM covid_deaths
 WHERE continent IS NULL
 GROUP BY location
 HAVING location = 'World';
 
--- check vaccinations vs population; rolling count of vaccinations
+-- Check vaccinations vs population; rolling count of vaccinations
+-- new_vaccinations column is incomplete; use new_vaccinations_smoothed column instead
+-- People vaccinated = total_vaccinations/ 2; 2 jabs for full vaccination
+-- METHOD 1: Views
 DROP VIEW IF EXISTS vac_vs_pop;
 
 CREATE VIEW vac_vs_pop AS (
-	SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations,
-	SUM(vac.new_vaccinations) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS rolling_total_vaccinations
-	FROM covid_deaths AS dea INNER JOIN covid_vaccinations AS vac
+	SELECT 
+		dea.continent, 
+		dea.location, 
+		dea.date, 
+		dea.population, 
+		vac.new_vaccinations,
+		vac.new_vaccinations_smoothed,
+		SUM(vac.new_vaccinations_smoothed) 
+			OVER (
+				PARTITION BY vac.location 
+				ORDER BY vac.location, vac.date
+			) 
+		AS rolling_total_vaccinations
+	FROM covid_deaths AS dea 
+	INNER JOIN covid_vaccinations AS vac
 	ON dea.location = vac.location
 	AND dea.date = vac.date
 	WHERE dea.continent IS NOT NULL
 	ORDER BY location, date
 );
 
-SELECT *, ROUND(rolling_total_vaccinations/population*100,2) AS vac_percent
+SELECT 
+	*, 
+	ROUND(rolling_total_vaccinations/2/population*100,2) AS vac_percent
 FROM vac_vs_pop
 ORDER BY location, date;
+
+-- Check vaccinations vs population; rolling count of vaccinations
+-- new_vaccinations column is incomplete; use new_vaccinations_smoothed column instead 
+-- People vaccinated = total_vaccinations/ 2; 2 jabs for full vaccination
+-- Method 2: CTE
+WITH vac_vs_pop AS (
+	SELECT 
+		dea.continent, 
+		dea.location, 
+		dea.date, 
+		dea.population, 
+		vac.new_vaccinations,
+		vac.new_vaccinations_smoothed,
+		SUM(vac.new_vaccinations_smoothed) 
+			OVER (
+				PARTITION BY dea.location 
+				ORDER BY dea.location, dea.date
+			) 
+		AS rolling_total_vaccinations
+	FROM covid_deaths AS dea 
+	INNER JOIN covid_vaccinations AS vac
+	ON dea.location = vac.location
+	AND dea.date = vac.date
+	WHERE dea.continent IS NOT NULL
+	ORDER BY location, date
+)
+SELECT 
+	*, 
+	ROUND(rolling_total_vaccinations/2/population*100,2) AS vac_percent
+FROM vac_vs_pop
+ORDER BY location, date;
+
--- covid-19_data_exploration/README.md
+++ covid-19_data_exploration/README.md
@@ -7,3 +7,5 @@ For this project, COVID-19 data from Our World in Data was extracted on 30-09-20
 The COVID-19 dataset from Our World in Data is an extremely comprehensive dataset containing information about deaths, cases, tests, hospitalizations, vaccinations, mortality risk and excess mortality for all countries in the world. Data is updated on a daily basis and provides an excellent resource to understand the COVID-19 situation for different places around the world, beyond what is seen and heard in the mainstream media.
 
 The data extracted was cleaned using the Python pandas library and separated into 2 tables, namely `covid_deaths` and `covid_vaccinations`. Both tables were subsequently imported into postgreSQL via pgAdmin for data exploration using SQL queries.
+
+Finally, Tableau was used to visualise the key findings into a dashboard. Access the full dashboard on Tableau Public using this link: https://public.tableau.com/views/COVID-19Dashboard_16338670691100/COVID-19Dashboard?:language=en-US&:display_count=n&:origin=viz_share_link
